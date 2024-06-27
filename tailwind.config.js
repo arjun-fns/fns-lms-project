71d44031d4b5eb3c2c3a1157ab18d10499f4b2ba
arjun <arjun.fns@gmail.com>
2024-06-27 17:22:43 +0530

    wildcard path change for tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
     parser: 'postcss-scss',
     content: [
-       "./auth-ui/*.{html,js}",
-       "./sanket/*.{html,js}",
-       "./sanket/alpinemenu/*.{html,js}",
-       "./manish/*.{html,js}",
+    //    "./auth-ui/*.{html,js}",
+    //    "./sanket/*.{html,js}",
+    //    "./sanket/alpinemenu/*.{html,js}",
+    //    "./manish/*.{html,js}",
+    "./*.html",       // All files in the root directory
+    "./*/*.html",     // All files in folders directly under the root directory
+    "./**/*.html",    // All files in any folder at any depth under the root directory
     ],
     theme: {
         extend: {},
