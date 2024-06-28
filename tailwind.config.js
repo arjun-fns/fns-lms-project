22c120397cb86a878913ef76c8c27d143a21dcc7
sanket <sanket.fns@gmail.com>
2024-06-28 17:35:59 +0530

    made changes for script flow added checkout page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    parser: 'postcss-scss',
-    content: [
-       "./auth-ui/*.{html,js}",
-       "./sanket/*.{html,js}",
-       "./sanket/alpinemenu/*.{html,js}",
-       "./manish/*.{html,js}",
-
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
-}
+  parser: "postcss-scss",
+  content: [
+    //    "./auth-ui/*.{html,js}",
+    //    "./sanket/*.{html,js}",
+    //    "./sanket/alpinemenu/*.{html,js}",
+    //    "./manish/*.{html,js}",
+    "./*.html", // All files in the root directory
+    "./*/*.html", // All files in folders directly under the root directory
+    "./**/*.html", // All files in any folder at any depth under the root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
