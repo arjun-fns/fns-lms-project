8c4a8c14ae0bf12dcbc5519542e27434d2ec97f9
arjun <arjun.fns@gmail.com>
2024-07-01 08:56:38 +0530

    refactor and updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,11 @@
 module.exports = {
     parser: 'postcss-scss',
     content: [
-    //    "./auth-ui/*.{html,js}",
-    //    "./sanket/*.{html,js}",
-    //    "./sanket/alpinemenu/*.{html,js}",
-    //    "./manish/*.{html,js}",
-    "./*.html",       // All files in the root directory
-    "./*/*.html",     // All files in folders directly under the root directory
-    "./**/*.html",    // All files in any folder at any depth under the root directory
+        //    "./sanket/alpinemenu/*.{html,js}",
+        //    "./lk-navbar/*.html",
+        "./*.html",       // All files in the root directory
+        "./*/*.html",     // All files in folders directly under the root directory
+        "./**/*.html",    // All files in any folder at any depth under the root directory
     ],
     theme: {
         extend: {},
